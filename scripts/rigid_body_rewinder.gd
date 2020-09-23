3d7e6c8f8e47926fb3408eeb2e77a54e2e67c619
Roy <roy.motloutsi@gmail.com>
2020-09-23 21:27:26 +0200

    fix(rigid_body_rewinder): revert physics server position overwrites
    Provide a correction for the physics server's property overwrites since it's unaware of this whole
    "rewind" business.

--- scripts/rigid_body_rewinder.gd
+++ scripts/rigid_body_rewinder.gd
@@ -1,21 +1,45 @@
 extends Rewinder
 
 onready var initial_mode = parent.mode
-func _process(delta):
+func _physics_process(delta):
+	# disable physics while rewinding
 	if RewindSystem.rewinding:
 		parent.mode = RigidBody2D.MODE_KINEMATIC
-	else:
+
+	elif !RewindSystem.rewinding and parent.mode != initial_mode:
+		"""
+			Not sure why this is, but the physics system tries to 
+			pick up where it left off before the RigidBody2D.mode
+			was set to KINEMATIC or whatever(i.e. not RIGID), and
+			this results in the physics system resetting the position
+			and forces to where it was before the rewind, overwriting
+			what I set during the rewind. 
+
+			So to fix this, I yield 1 physics frame(or however many it
+			yields, since the code seems to run twice before and twice after),
+			then if the distance between the current parent.global_position
+			and pre_pos is greater than my allowed margin, it means the physics
+			server has overwritten my values, and I overwrite it to the last correct
+			data. This means that rewinding a RigidBody2D will end at position n-1, 
+			instead of n, when rewinding since this overwrite happens 
+			most of the time.
+		"""
+		var pre_pos = parent.global_position
+		var pre_lin_vel = parent.linear_velocity
+		var pre_ang_vel = parent.angular_velocity
+		var pre_rot = parent.rotation
 		parent.mode = initial_mode
 
+		yield(get_tree(), "physics_frame")
+
+		if (pre_pos as Vector2).distance_to(parent.global_position) > 2:
+			parent.global_position = pre_pos
+			parent.linear_velocity = pre_lin_vel
+			parent.angular_velocity = pre_ang_vel 
+			parent.rotation = pre_rot
+
+
 func __record():
-	if !rewind_data.empty():
-		var prev = rewind_data[0]
-		var pos = parent.global_position as Vector2
-		if pos.distance_to(prev.pos) > 100:
-			# I cannot figure out what causes the code to get here,
-			# This is section is just to see part of the bug. This happens
-			# when rewinding the falling of the stack of boxes
-			print("problemo")
 	return {
 		pos = parent.global_position,
 		lin_vel = parent.linear_velocity,
